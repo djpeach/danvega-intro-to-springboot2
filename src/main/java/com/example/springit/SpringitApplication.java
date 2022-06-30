cc9b7c853285a4fca7844d7c592fb6c602d496d9
Daniel Peach <djpeach96@gmail.com>
2022-06-29 22:41:28 -0400

    added spotless

--- src/main/java/com/example/springit/SpringitApplication.java
+++ src/main/java/com/example/springit/SpringitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringitApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringitApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringitApplication.class, args);
+	}
 
 }
